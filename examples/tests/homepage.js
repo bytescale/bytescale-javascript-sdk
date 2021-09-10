b0ce603fc8407c2d5bc91c0ed7a427f7530600f8
Lawrence Wagerfield <lawrence@upload.io>
2021-09-10 20:53:23 +0000

    Fix test

--- examples/tests/homepage.js
+++ examples/tests/homepage.js
@@ -2,7 +2,7 @@ describe("Homepage", function () {
   it("should appear", function () {
     browser.get("/");
 
-    const btnBrowse = element(by.css('[data-qa="btn-browse"]'));
+    const btnBrowse = element(by.css("input"));
 
     expect(btnBrowse.isDisplayed()).toBeTruthy();
   });
