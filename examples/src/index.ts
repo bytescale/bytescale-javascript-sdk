1bf089aa4fc8dd9ef182f20814824b423906ff3b
Lawrence Wagerfield <lawrence@projectone.io>
2022-03-03 14:06:16 +0000

    Wrap expected errors into UploadError type

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -1,15 +1,13 @@
-import { Upload } from "upload-js";
+import { Upload, UploadError } from "upload-js";
 
 // For local development of Upload.js only: this is not an example of how you should set the API key in your app.
 const apiKey: string | undefined = (window as any).UPLOAD_JS_API_KEY;
-if (apiKey === undefined) {
-  throw new Error("You must set the environment variable 'UPLOAD_JS_API_KEY' before running webpack.");
-}
 
 const upload = new Upload({
-  apiKey: "free",
+  apiKey: apiKey ?? "free",
   internal: { apiUrl: (window as any).UPLOAD_JS_API_URL, cdnUrl: (window as any).UPLOAD_JS_CDN_URL }
 });
+
 const button = document.createElement("input");
 
 button.id = "upload"; // For acceptance testing
@@ -18,6 +16,13 @@ button.innerHTML = "Upload";
 button.onchange = upload.createFileInputHandler({
   onUploaded: ({ fileUrl }) => {
     console.log(`File uploaded to: ${fileUrl}`);
+  },
+  onError: error => {
+    if (error instanceof UploadError) {
+      alert(error.errorCode);
+    } else {
+      alert(error);
+    }
   }
 }) as any;
 
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,4 @@
 export { Upload } from "upload-js/Upload";
+export { UploadError } from "upload-js/UploadError";
 export { UploadConfig, UploadInternalConfig, UploadInternalConfigBase } from "upload-js/UploadConfig";
 export { UploadedFile } from "upload-js/UploadedFile";
