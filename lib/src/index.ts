999499876cf4dd11d1245e7ac6430eee5e2b41ec
Lawrence Wagerfield <lawrence@projectone.io>
2022-04-12 20:45:28 +0100

    Add FileInputChangeEvent to exports

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,3 +2,4 @@ export { Upload } from "upload-js/Upload";
 export { UploadError } from "upload-js/UploadError";
 export { UploadConfig, UploadInternalConfig, UploadInternalConfigBase } from "upload-js/UploadConfig";
 export { UploadedFile } from "upload-js/UploadedFile";
+export { FileInputChangeEvent } from "upload-js/FileInputChangeEvent";
