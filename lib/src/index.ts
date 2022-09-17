90be71d421f5b29b5cadd63ac1e772b404bf1f44
Lawrence Wagerfield <lawrence@projectone.io>
2022-09-17 09:30:28 +0100

    Export UploadInterface

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,4 +1,5 @@
 export { Upload } from "upload-js/Upload";
+export { UploadInterface } from "upload-js/UploadInterface";
 export { UploadApiError } from "upload-js/UploadApiError";
 export { UploadConfig, UploadInternalConfig, UploadInternalConfigBase } from "upload-js/UploadConfig";
 export { UploadedFile } from "upload-js/UploadedFile";
