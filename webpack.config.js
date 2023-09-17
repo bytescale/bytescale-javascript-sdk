aa71ad837a9f60a3a24bccd55e4b19b23cdc1039
Lawrence Wagerfield <lawrence@projectone.io>
2023-09-17 20:53:19 +0100

    Re-introduce ESM module for browsers

--- webpack.config.js
+++ webpack.config.js
@@ -78,26 +78,13 @@ const workerESM = {
   }
 };
 
-// We don't need the browser ESM module, because we use 'exports.default' to specify the browser environment, per the
-// Node.js documentation recomendations of specifying non-browser envs using guards ("node", "worker") and then using
-// "default" to effectively specify your "browser" environment. The "default" field only takes a string (i.e. it can't
-// use "import" or "require" guards, since it's the default, so can only specify one export). Therefore, since we
-// previously had a problem with browser ESM modules in Next.js (below), then we've selected the CJS module for our
-// browser export, which means we don't need the ESM export. Note: the error below might no-longer occur now that we
-// have "worker" exports to target the edge runtime.
-// ---------
-// Next.js gives 'ReferenceError: exports is not defined' from API routes when `export const config = { runtime: 'edge' }`
-// is used, due to it loading browser ESM modules, but for some reason for the browser modules specifically, it gives
-// 'exports is not defined', whereas the Node ESM module it's OK with. Anyhow, since Next.js is so popular, we disable
-// browser ESM modules, as Next.js is OK with the CommonJS module we export.
-// ---------
-// const browserESM = {
-//   ...baseESM,
-//   entry: "./src/index.browser.ts",
-//   output: {
-//     ...baseESM.output,
-//     path: path.join(__dirname, "dist/browser/esm")
-//   }
-// };
+const browserESM = {
+  ...baseESM,
+  entry: "./src/index.browser.ts",
+  output: {
+    ...baseESM.output,
+    path: path.join(__dirname, "dist/browser/esm")
+  }
+};
 
-module.exports = [nodeCJS, nodeESM, workerCJS, workerESM, browserCJS];
+module.exports = [nodeCJS, nodeESM, workerCJS, workerESM, browserCJS, browserESM];
