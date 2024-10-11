a8548431ffae3e0b81b0d83d6e0c24ee1c1569bc
Lawrence Wagerfield <lawrence@bytescale.com>
2024-10-11 12:00:34 +0100

    Use official "Readable.from" methods where possible

--- src/private/StreamUtils.ts
+++ src/private/StreamUtils.ts
@@ -3,29 +3,27 @@ import type * as stream from "stream";
 
 export class StreamUtils {
   static create(): stream.Readable {
-    const readable = new Readable();
-    readable._read = () => {}; // _read is required but you can noop it
-    return readable;
-  }
-
-  static empty(): NodeJS.ReadableStream {
-    const readable = StreamUtils.create();
-    StreamUtils.endStream(readable);
-    return readable;
-  }
-
-  static fromArrayBuffer(buffer: ArrayBuffer): NodeJS.ReadableStream {
-    return StreamUtils.fromBuffer(Buffer.from(buffer));
-  }
-
-  static fromBuffer(buffer: Buffer): NodeJS.ReadableStream {
-    const readable = StreamUtils.create();
-    readable.push(buffer);
-    StreamUtils.endStream(readable);
-    return readable;
+    return new Readable({
+      read(_size) {
+        // No implementation needed here.
+        // The stream will wait for data to be pushed externally.
+      }
+    });
   }
 
   static endStream(readable: stream.Readable): void {
     readable.push(null);
+  }
+
+  static empty(): NodeJS.ReadableStream {
+    return Readable.from([]);
+  }
+
+  static fromBuffer(buffer: Buffer): NodeJS.ReadableStream {
+    return Readable.from(buffer);
+  }
+
+  static fromArrayBuffer(buffer: ArrayBuffer): NodeJS.ReadableStream {
+    return StreamUtils.fromBuffer(Buffer.from(buffer));
   }
 }
